bf8af57a48ca73df96a7726521ae53590d6f4226
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-27 13:08:18 +0300

    lol

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@
 
 function sayHello() {
     console.log('hello world');
-}
+};
 //sayHello();
 
 var say = function() {
     console.log('Hello');
-}
+};
 //say();
 
 var a = 21;
@@ -16,7 +16,7 @@ var b = 2;
 
 function sum(x, y) {
     return x + y;
-}
+};
 
 //console.log(sum(a, b));
 //console.log(sum(2.4, 3.123));
@@ -26,6 +26,6 @@ var sayHi = function(name) {
     console.log('Hello, ' + name);
 };
 
-sayHi('Вася');
-sayHi('Андрей');
-sayHi('Джон');
+//sayHi('Вася');
+//sayHi('Андрей');
+//sayHi('Джон');
