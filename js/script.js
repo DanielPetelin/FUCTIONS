a11f1efd601e3edafef938af29353c340821abb5
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-27 12:54:55 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-console.log('hello world');
+//Функция 1 будет выводить в консоль 'hello world'
+//console.log('hello world');
 
 function sayHello() {
-
+    console.log('hello world');
 }
+
+sayHello();
